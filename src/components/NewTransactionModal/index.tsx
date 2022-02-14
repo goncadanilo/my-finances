1391d5187e01ee198609318cb163b1df940beb38
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-02-13 21:50:14 -0300

    feat: create buttons that define the transaction type

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from 'src/assets/close.svg';
-import { Container } from './styles';
+import incomeImg from 'src/assets/income.svg';
+import outcomeImg from 'src/assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -33,6 +35,18 @@ export function NewTransactionModal({
 
         <input placeholder="Título" />
         <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
