83d8996536e68f987e5caa9cae8450c28910ab85
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-02-17 23:09:31 -0300

    refactor: change import

--- src/TransactionContext.tsx
+++ src/TransactionContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
-import { api } from './services/api';
+import { api } from 'src/services/api';
 
 interface Transaction {
   id: number;
